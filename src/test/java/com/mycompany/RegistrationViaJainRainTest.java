195e8302eb0821d5dd54b2934ed57ed1b52b8ec1
Dmytro <dmazepa@speroteck.com>
2012-08-10 19:28:09 +0300

    second

--- src/test/java/com/mycompany/RegistrationViaJainRainTest.java
+++ src/test/java/com/mycompany/RegistrationViaJainRainTest.java
@@ -3,19 +3,21 @@ package com.mycompany;
 import com.mycompany.requirements.Application;
 import com.mycompany.steps.GuestSteps;
 import net.thucydides.core.annotations.*;
+import net.thucydides.core.pages.Pages;
 import net.thucydides.junit.runners.ThucydidesRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 
 @Story(Application.Registration.EasyRegistration.class)
 @RunWith(ThucydidesRunner.class)
 public class RegistrationViaJainRainTest {
 
     @ManagedPages(defaultUrl = "http://localhost:9000")
-
+    public Pages pages;
 
     @Managed(uniqueSession = true)
-
+    public WebDriver webdriver;
 
     @Steps
     public GuestSteps guest;

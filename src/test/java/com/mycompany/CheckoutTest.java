30ed7911184844a3933cd4b5b61ba32f9e0caf51
Dmytro <dmazepa@speroteck.com>
2012-10-03 16:42:48 +0300

    move to JUNIT

---
+++ src/test/java/com/mycompany/CheckoutTest.java
@@ -0,0 +1,35 @@
+package com.mycompany;
+
+import com.mycompany.steps.GuestSteps;
+import net.thucydides.core.annotations.*;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.junit.runners.ThucydidesRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(ThucydidesRunner.class)
+public class CheckoutTest {
+
+    @Managed
+    public WebDriver webdriver;
+
+    @ManagedPages(defaultUrl = "http://localhost:9000/dev01/")
+    public Pages pages;
+
+    @Steps
+    public GuestSteps steps;
+
+    @Pending @Test
+    public void as_guest_i_can_buy_bundle_product() throws InterruptedException {
+        //given
+        steps.is_the_product_page_id("91");
+        //when
+        steps.add_to_cart_product();
+        //and
+        steps.passing_checkout_process();
+        //then
+        steps.is_the_order_confirmation_page();
+        steps.assert_product_on_confirmation_page();
+    }
+}
---
+++ src/test/java/com/mycompany/CreateAccountTest.java
@@ -0,0 +1,37 @@
+package com.mycompany;
+
+import com.mycompany.steps.GuestSteps;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.WithDriver;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.junit.runners.ThucydidesRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(ThucydidesRunner.class)
+public class CreateAccountTest {
+
+    @Managed
+    public WebDriver webdriver;
+
+    @ManagedPages(defaultUrl = "http://localhost:9000/dev01/")
+    public Pages pages;
+
+    @Steps
+    public GuestSteps steps;
+
+    @Test
+    public void as_guest_i_can_create_account_from_home_page(){
+        //given
+        steps.is_the_home_page();
+        //when
+        steps.click_on_SignIn_link();
+        steps.click_on_Register_link();
+        steps.fill_fields_and_submit();
+        //then
+        steps.assert_created_account();
+    }
+}

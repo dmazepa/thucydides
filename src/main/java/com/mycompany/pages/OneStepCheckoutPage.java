d3b57abd80ed81d39dac8956f1615d08ed9fb458
Dmytro <dmazepa@speroteck.com>
2012-10-31 15:25:36 +0200

    checkout test

--- src/main/java/com/mycompany/pages/OneStepCheckoutPage.java
+++ src/main/java/com/mycompany/pages/OneStepCheckoutPage.java
@@ -143,7 +143,7 @@ public class OneStepCheckoutPage extends PageObject {
     }
 
     public void check_sipment() {
-        element(shipment).click();
+        element(shipment).waitUntilVisible();
         element(shipment).click();
     }
 }
